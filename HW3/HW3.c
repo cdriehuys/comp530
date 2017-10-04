3315841bbf73077f7a22592a577f0c6c25dc03e2
Chathan Driehuys <chathan@driehuys.com>
2017-10-04 00:20:27 -0400

    Add character replacement logic.
    Characters are now replaced correctly.
      * '\n' -> ' '
      * '**' -> '^'

--- HW3/HW3.c
+++ HW3/HW3.c
@@ -15,18 +15,23 @@
 #include "st.h"
 
 // Default streams
-#define DEFAULT_INPUT_STREAM stdin;
+#define INPUT_STREAM stdin;
+#define OUTPUT_STREAM stdout;
+
+// Other Settings
+#define LINE_LENGTH 80
 
 
 // Structure to hold thread info
 typedef struct {
-    FILE* input_stream;
-    FILE* output_stream;
-    buffer* buf;
+    buffer* input_buffer;
+    buffer* output_buffer;
 } ThreadInit;
 
 
 void* producer_logic(void* state);
+void* newline_replacer(void * state);
+void* asterisk_replacer(void * state);
 void* consumer_logic(void* state);
 
 
@@ -34,24 +39,42 @@ int main() {
     // Initialize ST library
     st_init();
 
-    // Set streams
-    FILE* input_stream = DEFAULT_INPUT_STREAM;
-    FILE* output_stream = stdout;
+    // Create shared buffers
+    buffer input_buffer;
+    initializeBuffer(&input_buffer);
 
-    // Create shared buffer
-    buffer buf;
-    initializeBuffer(&buf);
+    buffer input_buffer2;
+    initializeBuffer(&input_buffer2);
+
+    buffer input_buffer3;
+    initializeBuffer(&input_buffer3);
 
     // Create structure used to initialize threads
-    ThreadInit init = {
-        input_stream,
-        output_stream,
-        &buf
+    ThreadInit init_prod = {
+        NULL,
+        &input_buffer
+    };
+
+    ThreadInit init_newline = {
+        &input_buffer,
+        &input_buffer2
+    };
+
+    ThreadInit init_asterisk = {
+        &input_buffer2,
+        &input_buffer3
+    };
+
+    ThreadInit init_consumer = {
+        &input_buffer3,
+        NULL
     };
 
     // Create threads
-    st_thread_create(producer_logic, &init, 0, 0);
-    st_thread_create(consumer_logic, &init, 0, 0);
+    st_thread_create(producer_logic, &init_prod, 0, 0);
+    st_thread_create(newline_replacer, &init_newline, 0, 0);
+    st_thread_create(asterisk_replacer, &init_asterisk, 0, 0);
+    st_thread_create(consumer_logic, &init_consumer, 0, 0);
 
     // Wait for threads to complete
     st_thread_exit(NULL);
@@ -65,10 +88,10 @@ void* producer_logic(void* state) {
 
     while (1) {
         // Read next character from input stream
-        char c = fgetc(init->input_stream);
+        char c = fgetc(INPUT_STREAM);
 
         // Deposit it in the shared buffer to be processed
-        deposit(init->buf, c);
+        deposit(init->output_buffer, c);
 
         // If the character was EOF, terminate the thread.
         if (c == EOF) {
@@ -78,19 +101,81 @@ void* producer_logic(void* state) {
 }
 
 
-void* consumer_logic(void* state) {
+void* newline_replacer(void* state) {
     ThreadInit* init = state;
 
     while (1) {
         // Pull the next character from the buffer
-        char c = remoove(init->buf);
+        char c = remoove(init->input_buffer);
+
+        if (c == '\n') {
+            c = ' ';
+        }
+
+        deposit(init->output_buffer, c);
+
+        if (c == EOF) {
+            st_thread_exit(NULL);
+        }
+    }
+}
+
+
+void* asterisk_replacer(void* state) {
+    ThreadInit* init = state;
+
+    char last_char = '\0';
+
+    while (1) {
+        // Pull the next character from the buffer
+        char c = remoove(init->input_buffer);
+
+        if (c == '*' && last_char == '*') {
+            deposit(init->output_buffer, '^');
+            last_char = '\0';
+        } else {
+            if (last_char != '\0') {
+                deposit(init->output_buffer, last_char);
+            }
+
+            last_char = c;
+        }
+
+        if (c == EOF) {
+            deposit(init->output_buffer, c);
+
+            st_thread_exit(NULL);
+        }
+    }
+}
+
+
+void* consumer_logic(void* state) {
+    ThreadInit* init = state;
+
+    // Create a buffer to store a complete line
+    char line_buffer[LINE_LENGTH + 1];
+    line_buffer[LINE_LENGTH] = '\0';
+
+    int index = 0;
+
+    while (1) {
+        // Pull the next character from the buffer
+        char c = remoove(init->input_buffer);
 
         // Exit if we've reached EOF
         if (c == EOF) {
             st_thread_exit(NULL);
         }
 
-        fprintf(init->output_stream, "Consumer received: %c\n", c);
-        fflush(init->output_stream);
+        line_buffer[index] = c;
+
+        // If we've completed a full line, output it
+        if (index == LINE_LENGTH - 1) {
+            fprintf(OUTPUT_STREAM, "%s\n", line_buffer);
+            index = 0;
+        } else {
+            index += 1;
+        }
     }
 }
