9a6daa633dbde7cf2893840268625f0160689741
Chathan Driehuys <chathan@driehuys.com>
2017-10-03 23:23:10 -0400

    Document buffer implementation.

--- HW3/buffer.c
+++ HW3/buffer.c
@@ -1,35 +1,44 @@
+/**
+ * Implementation of a buffer.
+ *
+ * See 'buffer.h' for details.
+ */
+
 #include <stdio.h>
 #include "buffer.h"
 #include "semaphore.h"
 
 
 void initializeBuffer(buffer* buf) {
-    printf("Initializing buffer with %d cells\n", BUFFER_SIZE);
-
     buf->in = 0;
     buf->out = 0;
 
+    // Initialize semaphores for boundary conditions
     createSem(&buf->emptyCells, BUFFER_SIZE);
     createSem(&buf->fullCells, 0);
 }
 
 
 void deposit(buffer* buf, char c) {
+    // Ensure there is space in the buffer
     down(&buf->emptyCells);
 
     buf->data[buf->in] = c;
     buf->in = (buf->in + 1) % BUFFER_SIZE;
 
+    // Allow the deposited data to be consumed
     up(&buf->fullCells);
 }
 
 
 char remoove(buffer* buf) {
+    // Ensure there is an item in the buffer
     down(&buf->fullCells);
 
     char c = buf->data[buf->out];
     buf-> out = (buf->out + 1) % BUFFER_SIZE;
 
+    // Allow data to be placed in the cell that was just read
     up(&buf->emptyCells);
 
     return c;
--- HW3/buffer.h
+++ HW3/buffer.h
@@ -1,3 +1,9 @@
+/**
+ * A buffer intended to pass data between threads.
+ *
+ * Data in the buffer behaves like it is in a first-in-first-out queue.
+ */
+
 #ifndef _BUFFER_H_
 #define _BUFFER_H_
 
@@ -14,10 +20,42 @@ typedef struct buffer {
     semaphore fullCells;
 } buffer;
 
+/**
+ * Initializes an empty buffer. This must be called before using the buffer.
+ *
+ * Args:
+ *     buf:
+ *         A pointer to the buffer to initialize.
+ */
 void initializeBuffer(buffer* buf);
 
+/**
+ * Place a new character in the buffer.
+ *
+ * If there is no space in the buffer, the thread will wait until there is
+ * space available before continuing.
+ *
+ * Args:
+ *     buf:
+ *         A pointer to the buffer to place the character in.
+ *     c:
+ *         The character to insert into the buffer.
+ */
 void deposit(buffer* buf, char c);
 
+/**
+ * Remove the next character from the buffer.
+ *
+ * If there are no characters in the buffer, the thread will wait until the
+ * next character is placed in the buffer before continuing.
+ *
+ * Args:
+ *     buf:
+ *         A pointer to the buffer to remove a character from.
+ *
+ * Returns:
+ *     The next character in the buffer.
+ */
 char remoove(buffer* buf);
 
 #endif
