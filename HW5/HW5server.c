1634249c905642f606a0485133af1a3558bc68ca
Chathan Driehuys <chathan@driehuys.com>
2017-11-02 19:07:39 -0400

    Actually execute the command on the server.

--- HW5/HW5server.c
+++ HW5/HW5server.c
@@ -12,6 +12,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -21,6 +22,7 @@
 
 #define STRING_CHUNK_SIZE 32
 #define TEMP_FILE_NAME_SIZE 12
+#define WHITESPACE " \t\n"
 
 
 ServerSocket welcome_socket;
@@ -29,6 +31,7 @@ Socket connect_socket;
 
 void exec_command_service();
 char* read_socket_string();
+char** tokenize(char *line);
 
 
 int main(int argc, char* argv[]) {
@@ -90,9 +93,15 @@ void exec_command_service() {
             perror("fork");
             exit(EXIT_FAILURE);
         } else if (worker_pid == 0) {
-            freopen(tempfile, "w", stdout);
-            printf("Running '%s' on child process.\n", command);
-            exit(EXIT_SUCCESS);
+            freopen(tempfile, "a", stdout);
+            freopen(tempfile, "a", stderr);
+
+            char** args = tokenize(command);
+            int status = execvp(args[0], args);
+
+            perror("execution error");
+
+            exit(status);
         } else {
             waitpid(worker_pid, NULL, 0);
 
@@ -144,4 +153,47 @@ char* read_socket_string(Socket socket) {
     } while(c != '\0');
 
     return str;
+}
+
+
+/**
+ * Tokenize the provided input.
+ *
+ * This function splits the given line into 'arguments' where each argument is
+ * separated by whitespace.
+ *
+ * Function adapted from the following Stack Overflow answer:
+ * https://stackoverflow.com/a/11198630/3762084
+ *
+ * Args:
+ *     line:
+ *         A pointer to the string to tokenize. This string will be modified.
+ *
+ * Returns:
+ *     An array containing pointers to each argument. The last element in the
+ *     array is NULL.
+ */
+char** tokenize(char *line) {
+    char** tokenized = NULL;
+
+    // Perform the first split
+    char* current_token = strtok(line, WHITESPACE);
+    int num_spaces = 0;
+
+    while (current_token) {
+        // We have another token, so we need to expand our tokenized array
+        num_spaces++;
+        tokenized = realloc(tokenized, sizeof(char*) * num_spaces);
+
+        tokenized[num_spaces - 1] = current_token;
+
+        // Pull the next token, if any, from the above 'strtok' call.
+        current_token = strtok(NULL, WHITESPACE);
+    }
+
+    // After tokenizing the input, we need to add a NULL as the last entry
+    tokenized = realloc(tokenized, sizeof(char*) * (num_spaces + 1));
+    tokenized[num_spaces] = NULL;
+
+    return tokenized;
 }
